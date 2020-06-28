5f7290337cf5701751d8908e6d0e001fc97d65d4
sungg <zwsun1948@163.com>
2020-06-28 16:14:06 +0800

    deploy

---
+++ precache-manifest.9e0b4973cda3764d420f6bb9f6ddb09a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6efc051f5400ea69ba2da7bcec525c91",
+    "url": "/MyTally-website/index.html"
+  },
+  {
+    "revision": "b1b52bd9f4e3cc9e22fb",
+    "url": "/MyTally-website/static/css/main.a8125dae.chunk.css"
+  },
+  {
+    "revision": "fdeaf72ae9655d00551e",
+    "url": "/MyTally-website/static/js/2.8eb89db5.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/MyTally-website/static/js/2.8eb89db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b52bd9f4e3cc9e22fb",
+    "url": "/MyTally-website/static/js/main.46f74319.chunk.js"
+  },
+  {
+    "revision": "4356868e5953acbe4d7a",
+    "url": "/MyTally-website/static/js/runtime-main.c7e8556f.js"
+  }
+]);
